5fc53de7e932c6b2e73cfb74f6b9b68d7851505a
tkfk418 <tkfk418@naver.com>
2023-02-10 10:15:30 +0900

    updated

--- ml2.py
+++ ml2.py
@@ -77,7 +77,7 @@ def prediction2():
                             )
             ax.legend()
             ax.grid(True, which='major', c='gray', ls='-', lw=1, alpha=0.2)
-            ax.set_title('Prophet 모델')
+            ax.set_title('Prophet Graph')
             st.pyplot(fig)
    
 
@@ -164,7 +164,7 @@ def prediction2():
             ax.plot(results['RENT_GTN'], label='past')
             ax.plot(results['Forecast'],label='prediction')
             ax.legend()
-            plt.title('LSTM 모델')
+            plt.title('LSTM Graph')
             st.pyplot(fig)
     
 
