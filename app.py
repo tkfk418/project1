0bcfa71f66e5c05dbd8afe056921fbb76a5fa239
tkfk418 <tkfk418@naver.com>
2023-03-02 09:52:55 +0900

    updated

--- app.py
+++ app.py
@@ -19,13 +19,11 @@ def main():
     st.title('🏘️내 방, 어디👀?')
 
 
-    selected3 = option_menu(None, ["🏠Home", "🔎전월세 검색", "📊전세 예측",
-    '🤖챗봇', '💬건의사항'], 
+    selected3 = option_menu(None, ["🏠Home", "🔎전월세 검색", "📊전세 예측", '🤖챗봇', '💬건의사항'], 
             # icons=['house', 'cloud-upload', "list-task", 'gear'], 
             menu_icon="cast", default_index=0, orientation="horizontal",
             styles={
                 "container": {"padding": "0!important", "background-color": "#fafafa"},
-                "icon": {"color": "gray", "font-size": "15px"}, 
                 "nav-link": {"font-size": "15px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
                 "nav-link-selected": {"background-color": "#47C83E"},
         }
